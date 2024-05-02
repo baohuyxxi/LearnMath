3c3e152fc0d76b57c0ff0f690900436b0e7cb59e
Mai Bảo Huy <106733089+baohuyxxi@users.noreply.github.com>
2024-05-02 17:01:37 +0700

    remove project

--- src/components/TestComponent/TestComponent.tsx
+++
@@ -1,9 +0,0 @@
-import './TestComponent.scss';
-
-export const TestComponent = () => {
-    return(
-        <div className="test-component">
-            <h1>Test Component</h1>
-        </div>
-    )
-}
